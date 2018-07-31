e2e7f8ee0c57b1251439c1847fa0db5a4479e03f
WingHe0702 <38596676+WingHe0702@users.noreply.github.com>
2018-08-01 00:26:32 +0800

    initial project
    initial project-Wing

---
+++ SCM_bigdata/src/main/java/com/study/bigdata/bean/BaiduEntry.java
@@ -0,0 +1,23 @@
+package com.study.bigdata.bean;
+
+public class BaiduEntry {
+    public String name;
+    public Object value;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
